3ca7d0e1e545252fefb40e439337d2ab0ef08b6a
Gowtham S <project3690000@gmail.com>
2025-06-13 21:49:31 +0530

    playing with route methods

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express();
 
-app.use("/",(req, res) => {
-    res.send("This is listening in server")
+// app.use("/", (req, res) => {
+//     res.send("This is listening in server")
+// })
+
+app.post("/user",(req,res)=>{
+    res.send("This is post method")
+})
+app.delete("/user",(req,res)=>{
+    res.send("This is the delete method")
+})
+
+app.patch("/user",(req,res)=>{
+    res.send("This is a patch method")
+})
+
+app.get("/user", (req, res) => {
+    res.send({name:"gowtham",lastname:"s"})
 })
 
 app.listen(7777, () => {
