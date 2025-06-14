4d0245d32067d1cda148b271734b2b2de999456f
Gowtham S <project3690000@gmail.com>
2025-06-14 19:52:21 +0530

    Database connection established

--- src/app.js
+++ src/app.js
@@ -1,40 +1,17 @@
 const express = require('express');
-const { adminAuth, userAuth } = require('./middlewares/auth.js');
 const app = express();
+const connectDB = require('./config/database')
 
 
-// app.use("/", (req, res) => {
-//     res.send("This is listening in server")
-// })
+connectDB()
+    .then(() => {
+        console.log("Databse connection established...");
+        app.listen(7777, () => {
+            console.log('Sever is running on port')
 
-app.use("/admin",adminAuth);
+        })
+    })
+    .catch((err) => {
+        console.error("Databse cannot be connected...")
 
-app.use("/",(err,req,res,next)=>{
-    if(err){
-        res.status(500).send("Something went wrong")
-    }
-})
-
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("All data send")
-})
-
-app.post("/user",userAuth,(req,res)=>{
-    res.send("This is post method")
-})
-app.delete("/user",(req,res)=>{
-    res.send("This is the delete method")
-})
-
-app.patch("/user",(req,res)=>{
-    res.send("This is a patch method")
-})
-
-app.get("/user", (req, res) => {
-    res.send({name:"gowtham",lastname:"s"})
-})
-
-app.listen(7777, () => {
-    console.log('Sever is running on port')
-
-})
+    })
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const connectDB = async()=>{
+    const uri = "mongodb+srv://namaste:RNvJEMb73dBCFXPs@cluster0.471mwqs.mongodb.net/devTinder";
+}
+
+module.exports = connectDB;
