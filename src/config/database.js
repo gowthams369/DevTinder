6a0740ce133223d8b6e277160155e1ecc8f36674
Gowtham S <project3690000@gmail.com>
2025-06-15 10:11:54 +0530

    improved error handling

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectDB = async()=>{
+const connectDB = async () => {
+  try {
     const uri = "mongodb+srv://namaste:RNvJEMb73dBCFXPs@cluster0.471mwqs.mongodb.net/devTinder";
-}
+    await mongoose.connect(uri, {
+    });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    throw err;
+  }
+};
 
 module.exports = connectDB;
