1a04bb2e69a17aa0eb7e946a1f7e63be67fa7785
Gowtham S <project3690000@gmail.com>
2025-06-17 11:03:17 +0530

    Corrected small type error

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -6,7 +6,7 @@ const validateSignupData = (req) => {
     if (!firstName || !lastName) {
         throw new Error('Name is not valid')
     } else if (!validator.isEmail(emailId)) {
-        throw new Error('Email is valid')
+        throw new Error('Email is not valid');
     } else if (!validator.isStrongPassword(password)) {
         throw new Error('Please enter a strong Password')
     }
