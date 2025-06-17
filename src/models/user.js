3ad28bdd591472982c75591ed785e00aaa842e32
Gowtham S <project3690000@gmail.com>
2025-06-17 18:21:57 +0530

    Added handler methods in models

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -49,6 +51,25 @@ const userSchema = new mongoose.Schema({
         type: [String],
     }
 });
+
+userSchema.methods.getJWT = async function () {
+    const user = this
+    const token = await jwt.sign({ _id: user._id }, "Dev@Tinder$790", {
+        expiresIn: "7d"
+    });
+    return token;
+}
+
+userSchema.methods.validatePassword = async function (passwordInputbyUser) {
+    const user = this;
+    const passwordHash = user.password;
+    const isPasswordvalid = await bcrypt.compare(
+        passwordInputbyUser,
+        passwordHash
+    );
+    return isPasswordvalid;
+
+}
 
 module.exports = mongoose.model("User", userSchema);
 
