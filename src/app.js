b457741e6dd3e7559f0dc92d7fd0934695554998
Gowtham S <project3690000@gmail.com>
2025-06-17 12:09:30 +0530

    Added Login logic

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const app = express();
 const connectDB = require('./config/database');
 const User = require("./models/user");
 const { validateSignupData } = require('./utils/validation');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const user = require('./models/user');
 
 
 app.use(express.json())
@@ -13,7 +14,7 @@ app.post("/signup", async (req, res) => {
         //validate signup 
         validateSignupData(req);
 
-        const { firstName, lastName, emailId, age,password } = req.body;
+        const { firstName, lastName, emailId, age, password } = req.body;
         //hashing password
         const passwordHash = await bcrypt.hash(password, 10);
         console.log(passwordHash);
@@ -32,6 +33,26 @@ app.post("/signup", async (req, res) => {
     } catch (err) {
         console.error(err.message)
         res.status(400).send("Error in adding data")
+
+    }
+})
+
+app.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+        const user = await User.findOne({ emailId });
+        if (!user) {
+            throw new Error("Invalid Credentials");
+        }
+        const isPasswordvalid = await bcrypt.compare(password, user.password);
+        if (isPasswordvalid) {
+            res.send("Login sucessfull")
+        } else {
+            throw new Error("Invalid credential")
+        }
+
+    } catch (error) {
+        res.status(400).send(error.message)
 
     }
 })
