ef96fe11a38cd388f413da2153cbf309f395f748
Gowtham S <project3690000@gmail.com>
2025-06-18 18:08:32 +0530

    logout fuction added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const authRouter = express.Router();
-const { signUp, login } = require('../controller/authController');
+const { signUp, login, logout } = require('../controller/authController');
 
 
-authRouter.post("/signup", signUp);
-authRouter.post("/login", login);
+authRouter.post("/user/signup", signUp);
+authRouter.post("/user/login", login);
+authRouter.post("/user/logout",logout)
 
 module.exports = authRouter;
