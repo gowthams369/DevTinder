15127620cbdbd5490f2b44bc8775a7b3fe2e54ca
Gowtham S <project3690000@gmail.com>
2025-06-18 18:07:41 +0530

    Profile edit fuction added

--- src/controller/profilecontroller.js
+++ src/controller/profilecontroller.js
@@ -1,4 +1,6 @@
-
+const user = require('../models/user');
+const { validateProfileData } = require('../utils/validation');
+const { userAuth } = require('../middlewares/auth');
 
 const getProfile = async (req, res) => {
     try {
@@ -10,4 +12,27 @@ const getProfile = async (req, res) => {
     }
 }
 
-module.exports ={getProfile}
+const editProfile = async (req, res) => {
+    try {
+        if (!validateProfileData(req)) {
+            throw new Error("Invalid profile data")
+        }
+        const loggedInUser = req.user;
+        Object.keys(req.body).forEach((key) => {
+            loggedInUser[key] = req.body[key]
+        })
+        res.status(200).json({ message: "profile edited sucessfully",data:loggedInUser })
+        await loggedInUser.save()
+        console.log(loggedInUser);
+    } catch (error) {
+        res.status(400).send(error.message)
+
+    }
+}
+
+const forgetPassword = async(req,res)=>{
+    const {password}=req.body;
+
+}
+
+module.exports = { getProfile, editProfile,forgetPassword }
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const profileRouter = express.Router();
 const { userAuth } = require('../middlewares/auth');
-const { getProfile } = require('../controller/profilecontroller');
+const { getProfile, editProfile } = require('../controller/profilecontroller');
 
 
-profileRouter.get("/user/profile", userAuth, getProfile);
+profileRouter.get("/user/profile/view", userAuth, getProfile);
+profileRouter.patch("/user/profile/edit",userAuth,editProfile);
 
 
 module.exports = profileRouter;
