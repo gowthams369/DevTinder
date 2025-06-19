e33977dfebcbee4ecf39bac48fd7045fef4ae56f
Gowtham S <project3690000@gmail.com>
2025-06-19 11:40:09 +0530

    Connection request logic added

--- src/controller/requestController.js
+++ src/controller/requestController.js
@@ -1,8 +1,49 @@
-
+const ConnectionRequest = require('../models/connectionRequest')
+const User = require('../models/user')
 
 const sendConnection = async (req, res) => {
-    console.log("send connection request")
-    res.send("Request is sending")
+    try {
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+        const allowedStatus = ["ignored", "intrested"];
+        if (!allowedStatus.includes(status)) {
+            return res.status(400).json({ message: "Invalid status type:" + status })
+        }
+
+        const toUser = await User.findById({ _id: toUserId });
+        if (!toUser) {
+            return res.status(400).json({ message: "User not found" })
+        }
+
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or: [
+                { fromUserId, toUserId },
+                { fromUserId: toUserId, toUserId: fromUserId },
+            ],
+        })
+        if (existingConnectionRequest) {
+            return res.status(400).json({ message: "Connection alredy send" })
+        }
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        })
+
+        const data = await connectionRequest.save();
+
+        res.json({
+            message: "Connection request send sucessfully",
+            data,
+        })
+
+    } catch (error) {
+        res.status(400).send(error.message)
+
+    }
 }
 
-module.exports ={sendConnection};
+module.exports = { sendConnection }
